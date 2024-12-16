240a8c40d41664082b829a57fda39e61f8f97921
microHoffman <skozak@protonmail.com>
2024-12-16 10:25:38 +0000

    feat: add upgrade to stake button, use dates instead of epochs by default, upgrade graph x labels

--- constants/contracts.ts
+++ constants/contracts.ts
@@ -2,3 +2,4 @@ export const SECONDS_IN_EPOCH = 2_419_200 as const
 export const DAYS_IN_EPOCH = 28 as const // SECONDS_IN_EPOCH / SECONDS_IN_DAY (86_400)
 export const EPOCHS_IN_YEAR = 13 as const
 export const MAX_EPOCHS_IN_FUTURE = 130 as const // 10 * EPOCHS_IN_YEAR
+export const MIN_STAKE_DURATION_IN_EPOCH = EPOCHS_IN_YEAR
